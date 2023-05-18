13b8e969810ff18f1beeb66099f8a2cfb6b85d9b
Ayush Soun <58261824+ayushsoun@users.noreply.github.com>
2023-05-18 11:04:51 +0530

    Add files via upload

---
+++ Assignment9may/Assignment9may/Program.cs
@@ -0,0 +1,24 @@
+﻿using Assignment9may;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Assignment 10-May");
+
+        var calculator = new Calculator();
+        calculator.CalculatorFn();
+
+        var factorial = new Factorial();
+        factorial.FactorialN();
+
+        var palindrome = new Palindrome();
+        palindrome.CheckPalindrome();
+
+        var prime = new Prime();
+        prime.FindPrime();
+
+        var triangle = new Triangle();
+        triangle.GetTriangle();
+    }
+}
