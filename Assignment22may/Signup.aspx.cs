432bf1ae4b4c4c84882c99aa40457e06b0a4a7db
Ayush Soun <58261824+ayushsoun@users.noreply.github.com>
2023-05-24 13:45:05 +0530

    Add files via upload

---
+++ Assignment22may/Signup.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LoginSignUp
+{
+    public partial class Signup : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(@"Data Source"); 
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnRegister(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = System.Data.CommandType.Text;
+            cmd.CommandText = "insert into registration values(" + txtFname + "" + txtLname + "" + txtEmail
+                + "" + txtAge + "" + txtCity + "" + txtGender + ")";
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+    }
+}
